aeb60dafc7123a652565f2d1a6b5f01b2c357e4a
qidian <qidian990107@163.com>
2020-10-31 01:56:01 -0700

    Set up react native paper and navigation

--- App.js
+++ App.js
@@ -1,116 +1,200 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-import {Example} from 'myok-component';
+import Feed from './src/feed';
+import Detail from './src/detail';
+
+import Contacts from './src/views/drawer/Contacts';
+import Favorites from './src/views/drawer/Favorites';
+import Settings from './src/views/drawer/Settings';
+
+import Tab1 from './src/views/tabs/Tab1';
+import Tab2 from './src/views/tabs/Tab2';
+import Tab3 from './src/views/tabs/Tab3';
 
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  NavigationContainer,
+  DarkTheme,
+  DrawerActions,
+} from '@react-navigation/native';
+
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {useColorScheme, AppearanceProvider} from 'react-native-appearance';
+
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+const MaterialBottomTabs = createMaterialBottomTabNavigator();
+const MaterialTopTabs = createMaterialTopTabNavigator();
 
 const App = () => {
+  const colorScheme = useColorScheme();
+
+  const MyTheme = {
+    dark: false,
+    colors: {
+      primary: 'white',
+      background: 'white',
+      card: '#65509f',
+      text: 'white',
+      border: 'green',
+    },
+  };
+
+  const linking = {
+    prefixes: ['recipes://'],
+    config: {
+      screens: {
+        Feed: 'feed/:title',
+        Detail: 'detail/:foodName',
+        BottomTabs: {
+          path: 'bottom_tabs',
+          screens: {
+            Tab1: {
+              path: 'bTab1',
+              exact: true,
+            },
+            Tab2: {
+              path: 'bTab2',
+              exact: true,
+            },
+            Tab3: {
+              path: 'bTab3',
+              exact: true,
+            },
+          },
+        },
+        TopTabs: {
+          path: 'top_tabs',
+          screens: {
+            Tab1: {
+              path: 'tTab1',
+              exact: true,
+            },
+            Tab2: {
+              path: 'tTab2',
+              exact: true,
+            },
+            Tab3: {
+              path: 'tTab3',
+              exact: true,
+            },
+          },
+        },
+        Favorites: 'favorites/:user/:id',
+        Contacts: 'contacts/:user?',
+        Settings: {
+          path: 'settings/:color/:age/:isVerified',
+          parse: {
+            age: Number,
+            isVerified: Boolean,
+            color: (color) => `color-${color}`,
+          },
+        },
+      },
+    },
+  };
+
+  const createHomeStack = () => (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Feed"
+        children={createDrawer}
+        options={({navigation}) => ({
+          title: 'React Navigation',
+          headerLeft: () => (
+            <Icon
+              onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+              style={[{color: 'white', marginLeft: 8}]}
+              size={24}
+              name={'menu'}
+            />
+          ),
+        })}
+      />
+      <Stack.Screen
+        name="Detail"
+        component={Detail}
+        options={{
+          title: 'Detail Screen',
+        }}
+      />
+      <Stack.Screen name="BottomTabs" component={BottomTabs} />
+      <Stack.Screen name="TopTabs" children={createTopTabs} />
+    </Stack.Navigator>
+  );
+
+  const createDrawer = () => (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="Contacts" component={Contacts} />
+      <Drawer.Screen name="Favorites" component={Favorites} />
+      <Drawer.Screen name="Settings" component={Settings} />
+    </Drawer.Navigator>
+  );
+
+  const createTopTabs = (props) => {
+    return (
+      <MaterialTopTabs.Navigator>
+        <MaterialTopTabs.Screen
+          name="Tab1"
+          component={Tab1}
+          options={{title: props.route.params.name}}
+        />
+        <MaterialTopTabs.Screen name="Tab2" component={Tab2} />
+        <MaterialTopTabs.Screen name="Tab3" component={Tab3} />
+      </MaterialTopTabs.Navigator>
+    );
+  };
+
+  const BottomTabs = () => {
+    return (
+      <MaterialBottomTabs.Navigator>
+        <MaterialBottomTabs.Screen
+          name="Tab1"
+          style={{marginBottom: 16}}
+          component={Tab1}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: () => (
+              <Icon style={[{color: 'white'}]} size={25} name={'home'} />
+            ),
+          }}
+        />
+        <MaterialBottomTabs.Screen
+          name="Tab2"
+          component={Tab2}
+          options={{
+            tabBarLabel: 'Profile',
+            tabBarIcon: () => (
+              <Icon style={[{color: 'white'}]} size={25} name={'human'} />
+            ),
+          }}
+        />
+        <MaterialBottomTabs.Screen
+          name="Tab3"
+          component={Tab3}
+          options={{
+            tabBarLabel: 'Map',
+            tabBarIcon: () => (
+              <Icon style={[{color: 'white'}]} size={25} name={'map'} />
+            ),
+          }}
+        />
+      </MaterialBottomTabs.Navigator>
+    );
+  };
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <Example />
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <AppearanceProvider>
+      <NavigationContainer
+        theme={colorScheme === 'dark' ? DarkTheme : MyTheme}
+        linking={linking}>
+        {createHomeStack()}
+      </NavigationContainer>
+    </AppearanceProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
