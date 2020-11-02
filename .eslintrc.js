629541aa14f637cdc1d955623a1b319fae3a0ed4
qidian <qidian990107@163.com>
2020-11-02 09:52:21 -0800

    Adde eslintrc from RN community

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,35 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  env: {
+    browser: true,
+    'react-native/react-native': true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-native'],
+  rules: {
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+    'react-native/sort-styles': [
+      'error',
+      'asc',
+      {
+        ignoreClassNames: false,
+        ignoreStyleProperties: false,
+      },
+    ],
+  },
 };
