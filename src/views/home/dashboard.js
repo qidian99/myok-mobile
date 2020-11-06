92e744260990a1da5aeccce008e27172785dac8c
EnqiZhang99 <enz004@ucsd.edu>
2020-11-06 15:54:10 -0800

    Refactoring Dashboard

--- src/views/home/dashboard.js
+++ src/views/home/dashboard.js
@@ -1,11 +1,51 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import {globalStyles} from 'styles/index';
+import {View, Text, FlatList} from 'react-native';
+import {useTheme} from 'react-native-paper';
 
-const Dashboard = () => (
-  <View style={globalStyles.center}>
-    <Text style={globalStyles.title}>Dashboard</Text>
-  </View>
-);
+const styles = {
+  containerStyle: {
+    margin: 24,
+  },
+  recentDocContainerStyle: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  recentDocTextStyle: {
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+  recentDocViewAllStyle: {
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: '#7CB0D7',
+  },
+};
+
+const renderDocumentCard = ({item}) => {
+  const {document_id, title, created_by} = item;
+  return <Text>Test</Text>;
+};
+
+const Dashboard = () => {
+  const {colors} = useTheme();
+  return (
+    <View style={styles.containerStyle}>
+      <View style={styles.recentDocContainerStyle}>
+        <Text style={[styles.recentDocTextStyle, {color: colors.primary}]}>
+          Recent Document
+        </Text>
+        <Text style={styles.recentDocViewAllStyle}>VIEW ALL</Text>
+      </View>
+      <FlatList
+        data={[]}
+        renderItem={renderDocumentCard}
+        keyExtractor={(item) => item.document_id}
+      />
+    </View>
+  );
+};
 
 export default Dashboard;
