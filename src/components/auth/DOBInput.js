e69a975471fb055830892f5f43dbf37f22f829e6
qidian <qidian990107@163.com>
2020-11-15 15:40:51 -0800

    Added menu options

--- src/components/auth/DOBInput.js
+++ src/components/auth/DOBInput.js
@@ -1,22 +1,61 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useCallback, useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import MaterialIcon from 'react-native-vector-icons/dist/MaterialIcons';
-
+import {Button, Menu, Divider, Provider} from 'react-native-paper';
 const DOBInput = ({title = 'Date of Birth'}) => {
   const {
     containerStyle,
     titleStyle,
     inputContainerStyle,
+    entryStyle,
     entryContainerStyle,
     entryTextStyle,
   } = styles;
+
+  const [visible, setVisible] = useState(false);
+
+  const [month, setMonth] = useState('Month');
+
+  const onOptionPress = useCallback(
+    (text, setText) => () => {
+      setText(text);
+      closeMenu();
+    },
+    [closeMenu],
+  );
+
+  const openMenu = () => setVisible(true);
+
+  const closeMenu = () => setVisible(false);
+
   return (
     <View style={containerStyle}>
       <Text style={titleStyle}>{title}</Text>
       <View style={inputContainerStyle}>
         <View style={entryContainerStyle}>
-          <Text style={entryTextStyle}>Month</Text>
-          <MaterialIcon name="expand-more" color="#2374A5" size={24} />
+          <Menu
+            visible={visible}
+            onDismiss={closeMenu}
+            anchor={
+              <TouchableOpacity style={entryStyle} onPress={openMenu}>
+                <Text style={entryTextStyle}>{month}</Text>
+                <MaterialIcon name="expand-more" color="#2374A5" size={24} />
+              </TouchableOpacity>
+            }>
+            <Menu.Item
+              onPress={onOptionPress('January', setMonth)}
+              title="January"
+            />
+            <Menu.Item
+              onPress={onOptionPress('February', setMonth)}
+              title="February"
+            />
+            <Divider />
+            <Menu.Item
+              onPress={onOptionPress('March', setMonth)}
+              title="March"
+            />
+          </Menu>
         </View>
         <View style={entryContainerStyle}>
           <Text style={entryTextStyle}>Day</Text>
@@ -44,6 +83,13 @@ const styles = StyleSheet.create({
     paddingHorizontal: 8,
     paddingVertical: 8,
     width: '30%',
+  },
+  entryStyle: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 8,
+    width: '100%',
   },
   entryTextStyle: {
     color: '#2374A5',
